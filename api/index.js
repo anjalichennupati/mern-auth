42d0150005f9ba8d265449f159c22c0838a4d21d
Anjali Chennupati <anjali.m.chennupati@gmail.com>
2023-12-12 22:59:56 +0530

    dotenv

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app= express();
-app.listen(3000, () => {
-    console.log('Server listening to port 3000');
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+app.listen(3000,() => {
+  console.log('Server is connecting to 3000');
+})
