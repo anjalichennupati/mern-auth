567ecd2f8f9de318ae69542e3f20a8cf1bc8b4d5
Anjali Chennupati <anjali.m.chennupati@gmail.com>
2023-12-12 20:44:20 +0530

    add commit and push

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from 'E:/3rd Year/5th Sem/DBMS/End Sem/mern-auth/client/src/pages/Profile.jsx';
 
 export default function App() {
   return (
-    <h1 className='text-yellow-500'>App</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
